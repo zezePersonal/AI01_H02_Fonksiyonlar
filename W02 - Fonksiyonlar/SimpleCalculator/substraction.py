a98249bffa0292c531db0a57ff4bdd4533ba0d4d
zezecan <42096225+zezecan@users.noreply.github.com>
2025-02-01 12:50:55 +0000

    20250201 H02D01

---
+++ W02 - Fonksiyonlar/SimpleCalculator/substraction.py
@@ -0,0 +1,2 @@
+def substraction(n1, n2:int):
+    return n1-n2
---
+++ W02 - Fonksiyonlar/main.py
@@ -0,0 +1,11 @@
+from SimpleCalculator.addition import addition
+from SimpleCalculator.substraction import substraction
+
+
+i = addition(2,3)
+print(i)
+
+j = substraction(10,1)
+print(j)
+
+# Hocadaki ornek input ile 
